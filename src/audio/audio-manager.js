af12491c5f2ec06da20d7b1d4e78ebc4f5e9026a
taha <shajariantaha@gmial.com>
2021-10-11 03:23:12 +0330

    bug fix

--- src/audio/audio-manager.js
+++ src/audio/audio-manager.js
@@ -38,13 +38,7 @@ export default class AudioManager {
 		allMusic.forEach(music => {
 			music.setMute(this.scene.config.isMute);
 		})
-		if (this.scene.config.isMute) {
-			this.soundOn.setTexture('sound-off').setScale(0.3).
-				setPosition(105, 28)
-		} else {
-			this.soundOn.setTexture('sound-on').setScale(0.5)
-				.setPosition(105, 20)
-		}
+		this.setPositionSpeaker()
 	}
 
 	createSoundControlButton() {
@@ -53,28 +47,31 @@ export default class AudioManager {
 			.image(80, 20, "sound").setDepth(100)
 			.setInteractive()
 			.setOrigin(0)
-			.setScale(0.5)
+			.setScale(0.3)
 			.setScrollFactor(0)
 		this.soundOn = this.scene.add
 			.image(105, 20, "sound-on").setDepth(100)
 			.setOrigin(0)
-			.setScale(0.5)
 			.setScrollFactor(0)
 
-		if (this.scene.config.isMute) {
-			this.soundOn.setTexture('sound-off').setScale(0.3).
-				setPosition(105, 28)
-		} else {
-			this.soundOn.setTexture('sound-on').setScale(0.5)
-				.setPosition(105, 20)
-		}
+		this.setPositionSpeaker();
 
 		this.soundControlButton = this.scene.add.container(0, 0, [this.soundBase, this.soundOn])
 			.setScrollFactor(0)
 			.setDepth(100)
+			.setAlpha(0.6)
 
 		this.soundBase.on("pointerdown", () => {
 			this.toggleSound();
 		});
 	}
+
+	setPositionSpeaker() {
+		if (this.scene.config.isMute) {
+			this.soundOn.setTexture('sound-off').setScale(0.25).
+				setPosition(98, 22)
+		} else {
+			this.soundOn.setTexture('sound-on').setScale(0.4)
+				.setPosition(98, 17)
+		}	}
 }
